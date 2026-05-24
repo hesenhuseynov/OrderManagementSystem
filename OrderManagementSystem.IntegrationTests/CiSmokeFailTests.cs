ebaab4ff344341d74f332702688e37d51393d0ba
Hesen Huseynov <gorbagor1522@gmail.com>
2026-05-24 06:44:11 +0400

    Test CI protection with falling test

---
+++ OrderManagementSystem.IntegrationTests/CiSmokeFailTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderManagementSystem.IntegrationTests
+{
+    public class CiSmokeFailTests
+    {
+        [Fact]
+        public void Ci_Should_Fail_Intentionally()
+        {
+            Assert.Fail("Intentional CI failure test");
+        }
+    }
+}
