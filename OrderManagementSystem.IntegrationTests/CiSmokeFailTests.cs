d27876078871f32be6a34059d394e2db74c88822
Hesen Huseynov <gorbagor1522@gmail.com>
2026-05-24 06:16:55 +0400

    Intentionally break integration test

---
+++ OrderManagementSystem.IntegrationTests/CiSmokeFailTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderManagementSystem.IntegrationTests
+{
+    public  class CiSmokeFailTests
+    {
+        [Fact]
+        public  void Ci_Should_Fail_Intentionaly()
+        {
+            Assert.Fail("Intentional CI Failure Test");
+        }
+    }
+}
